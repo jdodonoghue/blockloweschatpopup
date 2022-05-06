eb7d2f64aafc161b68cb827a056d6e5bfe187783
John O'Donoghue <jdodonoghue@yahoo.com>
2022-05-06 15:13:53 -0400

    Add project

---
+++ blockchatpopup.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+
+function hideChat(){
+    document.getElementById("chat_container").style.visibility = "hidden";
+}
+
+//run first
+setTimeout(hideChat, 5000);
+
+//run second time if page loads slowly
+setTimeout(hideChat, 10000);
+
+// third
+window.addEventListener('mousemove', function() {
+    hideChat();
+});
+
+//forth try
+window.addEventListener('click', function() {    
+    hideChat();
+});
+
+/*
+function onReady(){
+    console.log("start 1");
+
+}
+
+if (document.readyState !== "loading") {
+    onReady();
+} else {
+    console.log("start 2");
+}
+*/
+
+
+// need some improvment.. ignore functions after first succesful, better: detect end of page scripts to run hideChat() 
